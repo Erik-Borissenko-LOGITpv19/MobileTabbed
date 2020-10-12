cadf8a16a9ccb5f504e462925c325e529089b986
opilane <opilane@HP-CZC6297B8Z>
2020-10-12 15:04:55 +0300

    Add project files.

---
+++ MobileTabbed/MobileTabbed/Veebruar.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileTabbed
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Veebruar : TabbedPage
+    
+    {
+
+
+        public Veebruar()
+      {
+            InitializeComponent();
+        Grid grd = new Grid
+        {
+
+        };
+            for (int i = 0; i < 1; i++)
+            {
+                grd.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            }
+            for (int i = 0; i < 1; i++)
+            {
+                grd.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            }
+            {
+                BackgroundColor = Color.LightBlue;
+            }
+            Image img = new Image { Source = "Snegovik.png" };
+            grd.Children.Add(img, 0, 0);
+        }
+
+    }
+
+}
